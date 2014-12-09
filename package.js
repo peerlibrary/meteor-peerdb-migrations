b6a3da379876176832aca0ee94d7ddfc70b1ae1e
Mitar <mitar.git@tnode.com>
2014-12-09 05:30:53 -0800

    Initial version.

---
+++ package.js
@@ -0,0 +1,26 @@
+Package.describe({
+  summary: "PeerDB migrations.",
+  version: '0.1.0',
+  name: 'peerlibrary:peerdb-migrations',
+  git: 'https://github.com/peerlibrary/meteor-peerdb-migrations.git'
+});
+
+Package.on_use(function (api) {
+  api.versionsFrom('METEOR@1.0');
+  api.use(['coffeescript', 'underscore', 'minimongo', 'peerlibrary:assert@0.2.5', 'logging', 'peerlibrary:util@0.2.3', 'mrt:moment@2.8.1', 'peerlibrary:directcollection@0.2.2', 'peerlibrary:peerdb@0.15.0'], 'server');
+  api.add_files([
+    'server.coffee'
+  ], 'server');
+});
+
+Package.on_test(function (api) {
+  api.use(['peerlibrary:peerdb@0.15.0', 'peerlibrary:peerdb-migrations', 'tinytest', 'test-helpers', 'coffeescript', 'insecure', 'accounts-base', 'accounts-password', 'peerlibrary:assert@0.2.5', 'underscore', 'peerlibrary:directcollection@0.2.2', 'random'], ['client', 'server']);
+  api.add_files([
+    'tests_defined.js',
+    'tests.coffee'
+  ], ['client', 'server']);
+
+  api.add_files([
+    'tests_migrations.coffee'
+  ], 'server');
+});
---
+++ tests_defined.js
@@ -0,0 +1,11 @@
+Tinytest.add('peerdb-migrations - defined', function (test) {
+  var isDefined = false;
+  try {
+    Document;
+    isDefined = true;
+  }
+  catch (e) {
+  }
+  test.isTrue(isDefined, "Document is not defined");
+  test.isTrue(Package['peerlibrary:peerdb'].Document, "Package.peerlibrary:peerdb.Document is not defined");
+});
