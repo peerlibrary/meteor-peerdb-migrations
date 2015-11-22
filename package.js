bf3325562f749933c16eda53ca977e4bcd7101bf
Mitar <mitar.git@tnode.com>
2015-11-22 15:41:29 -0800

    Updated PeerDB dependency.

--- package.js
+++ package.js
@@ -7,14 +7,14 @@ Package.describe({
 
 Package.on_use(function (api) {
   api.versionsFrom('METEOR@1.0');
-  api.use(['coffeescript', 'underscore', 'minimongo', 'peerlibrary:assert@0.2.5', 'logging', 'peerlibrary:util@0.2.3', 'mrt:moment@2.8.1', 'peerlibrary:directcollection@0.5.0', 'peerlibrary:peerdb@0.15.1'], 'server');
+  api.use(['coffeescript', 'underscore', 'minimongo', 'peerlibrary:assert@0.2.5', 'logging', 'peerlibrary:util@0.2.3', 'mrt:moment@2.8.1', 'peerlibrary:directcollection@0.5.0', 'peerlibrary:peerdb@0.19.2'], 'server');
   api.add_files([
     'server.coffee'
   ], 'server');
 });
 
 Package.on_test(function (api) {
-  api.use(['peerlibrary:peerdb@0.15.1', 'peerlibrary:peerdb-migrations', 'tinytest', 'test-helpers', 'coffeescript', 'insecure', 'accounts-base', 'accounts-password', 'peerlibrary:assert@0.2.5', 'underscore', 'peerlibrary:directcollection@0.5.0', 'random', 'logging', 'ejson', 'mongo'], ['client', 'server']);
+  api.use(['peerlibrary:peerdb@0.19.2', 'peerlibrary:peerdb-migrations', 'tinytest', 'test-helpers', 'coffeescript', 'insecure', 'accounts-base', 'accounts-password', 'peerlibrary:assert@0.2.5', 'underscore', 'peerlibrary:directcollection@0.5.0', 'random', 'logging', 'ejson', 'mongo'], ['client', 'server']);
   api.add_files([
     'tests_defined.js',
     'tests.coffee'
