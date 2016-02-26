a1d809f77c16f4303f3f907cc851027a182fbb2b
Mitar <mitar.git@tnode.com>
2016-02-26 15:35:14 -0800

    Version bump.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'peerlibrary:peerdb-migrations',
   summary: "PeerDB migrations.",
-  version: '0.1.1',
+  version: '0.2.0',
   git: 'https://github.com/peerlibrary/meteor-peerdb-migrations.git'
 });
 
